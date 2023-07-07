e66fb356beb793d204e5f1da56317bbe7be1cb21
Daniel Chen <danieljchen@gmail.com>
2023-07-07 11:04:12 -0400

    add .angular cache

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'measured'`, () => {
+  it(`should have as title 'marketing-dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('measured');
+    expect(app.title).toEqual('marketing-dashboard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('measured app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('marketing-dashboard app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   providers: [DecimalPipe]
 })
 export class AppComponent {
-  title = 'measured';
+  title = 'marketing-dashboard';
 }
